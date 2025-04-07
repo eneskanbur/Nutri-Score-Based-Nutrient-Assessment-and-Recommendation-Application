71d1325bf9d1f1b92e6dd70fc9741c023c4ddfce
Talha Güneş <20soft1019@isik.edu.tr>
2025-04-07 21:57:07 +0300

    Login page tasarımı yapıldı.

---
+++ app/src/main/java/com/knbrgns/nutriscoreapp/ui/SignInPageFragment.kt
@@ -0,0 +1,19 @@
+package com.knbrgns.nutriscoreapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.knbrgns.nutriscoreapp.R
+
+class SignInPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_loginpage, container, false)
+    }
+
+}
